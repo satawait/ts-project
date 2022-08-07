9eef92ed844d8c0bc2e6a361d8600a101d377c1d
20201206-013040\Administrator <xingbangl@gmail.com>
2022-08-07 23:31:25 +0800

    study

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // // 关闭webpack箭头函数
+        // environment: {
+        //     arrowFunction: false
+        // }
+    },
+    module: {
+        rules:[
+            {
+                test: /\.ts$/,
+                use: [{
+                    loader: 'babel-loader'
+                }, 'ts-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [[
+                                'postcss-preset-env',
+                                {
+                                    browsers: 'last 2 versions'
+                                }
+                            ]]
+                        }
+                    }
+                }, 'less-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new htmlWebpackPlugin({
+            // title: 'ts-study'
+            template: './index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            '@': '/src/'
+        }
+    }
+}
